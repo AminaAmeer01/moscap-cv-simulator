6f680a8f1e08064d14381df904effafd008a352b
AminaAmeer01 <mina.riz1995@gmail.com>
2026-03-17 18:16:27 +0100

    Add depletion width calculation

--- src/physics/moscap.py
+++ src/physics/moscap.py
@@ -17,3 +17,25 @@ def oxide_capacitance(area: float, tox: float = tox_default) -> float:
         Capacitance in Farads
     """
     return epsilon_ox * area / tox
+
+
+from .constants import epsilon_si, q
+
+
+def depletion_width(phi_s: float, N_A: float) -> float:
+    """
+    Compute depletion width in a p-type semiconductor.
+
+    Parameters
+    ----------
+    phi_s : float
+        Surface potential (V)
+    N_A : float
+        Acceptor doping concentration (1/m^3)
+
+    Returns
+    -------
+    float
+        Depletion width (m)
+    """
+    return (2 * epsilon_si * phi_s / (q * N_A)) ** 0.5
