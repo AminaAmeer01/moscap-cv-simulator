06596d5a390a41d07920601139c04f7a3dcdc85b
AminaAmeer01 <mina.riz1995@gmail.com>
2026-03-17 17:50:14 +0100

    Add example script for intrinsic carrier concentration plot

---
+++ examples/plot_intrinsic_carrier.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from src.physics.semiconductor import intrinsic_carrier_concentration
+
+# Temperature range
+T = np.linspace(200, 400, 100)
+
+# Compute ni(T)
+ni = [intrinsic_carrier_concentration(t) for t in T]
+
+# Plot
+plt.figure()
+plt.plot(T, ni)
+plt.xlabel("Temperature (K)")
+plt.ylabel("Intrinsic Carrier Concentration (1/m^3)")
+plt.title("Intrinsic Carrier Concentration vs Temperature")
+plt.grid()
+
+plt.show()
