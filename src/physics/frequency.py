f7d67ac5f3a36624ecee53ceafc946e07dcb48f0
AminaAmeer01 <mina.riz1995@gmail.com>
2026-06-01 23:37:07 +0200

    Improve documentation of frequency-dependent MOS models

--- src/physics/frequency.py
+++ src/physics/frequency.py
@@ -1,6 +1,42 @@
+"""
+Frequency-dependent MOS capacitance models.
+"""
+
+
 def high_frequency_capacitance(Cox, Cs):
+    """
+    High-frequency MOS capacitance.
+
+    Minority carriers cannot respond to the AC signal.
+    The measured capacitance is the series combination
+    of oxide and depletion capacitances.
+
+    Parameters
+    ----------
+    Cox : float
+        Oxide capacitance
+    Cs : float
+        Semiconductor capacitance
+
+    Returns
+    -------
+    float
+        High-frequency capacitance
+    """
+
     return 1 / (1 / Cox + 1 / Cs)
 
 
 def low_frequency_capacitance(Cox):
+    """
+    Low-frequency MOS capacitance.
+
+    Minority carriers follow the AC signal.
+
+    Returns
+    -------
+    float
+        Low-frequency capacitance
+    """
+
     return Cox
